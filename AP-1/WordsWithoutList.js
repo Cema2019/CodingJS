21fe87a1a2dfa8e4f5fe594894c9b34303e1585d
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-02-23 11:13:05 -0600

    Create WordsWithoutList.js

---
+++ AP-1/WordsWithoutList.js
@@ -0,0 +1,12 @@
+/*AP-1 -- wordsWithoutList
+Given an array of strings, return a new List (e.g. an ArrayList) where all the strings of the given length are omitted. See wordsWithout() below which is more difficult because it uses arrays.
+
+Examples
+
+wordsWithoutList(['a', 'bb', 'b', 'ccc'], 1) → bb,ccc
+wordsWithoutList(['a', 'bb', 'b', 'ccc'], 3) → a,bb,b
+wordsWithoutList(['a', 'bb', 'b', 'ccc'], 4) → a,bb,b,ccc*/
+
+function wordsWithoutList(words, len){
+  return words.filter((word) => word.length !== len);
+}
