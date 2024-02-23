e9e5ffd226a7af0482af5aa6d9d5889369e2ccaf
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-02-23 11:25:41 -0600

    Create hasOne.js

---
+++ AP-1/hasOne.js
@@ -0,0 +1,12 @@
+/*AP-1 -- hasOne
+Given a positive int n, return true if it contains a 1 digit. Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+
+Examples
+
+hasOne(10) → true
+hasOne(22) → false
+hasOne(220) → false*/
+
+function hasOne(n){
+  return n.toString().includes(1);
+}
