4b3f19f202a685e248ffd7fa0be7950d344d1ea5
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-02-25 19:18:54 -0600

    Create lastChars.js

---
+++ String-1/lastChars.js
@@ -0,0 +1,12 @@
+/*String-1 -- lastChars
+Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+
+Examples
+
+lastChars('last', 'chars') → ls
+lastChars('yo', 'java') → ya
+lastChars('hi', '') → h@*/
+
+function lastChars(a, b){
+  return (a.length >= 1 ? a.charAt(0) : '@') + (b.length >= 1 ? b.charAt(b.length - 1) : '@');
+}
