618eaf825145e6bbef4454e3467411ee9b42b1fa
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-02-26 14:56:36 -0600

    Create mixStart.js

---
+++ Warmup-1/mixStart.js
@@ -0,0 +1,12 @@
+/*Warmup-1 -- mixStart
+Return true if the given string begins with 'mix', except the 'm' can be anything, so 'pix', '9ix' .. all count.
+
+Examples
+
+mixStart('mix snacks') → true
+mixStart('pix snacks') → true
+mixStart('piz snacks') → false*/
+
+function mixStart(str){
+  return /^\wix/.test(str);
+}
