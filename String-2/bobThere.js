df04f105490854efb49a563f8ea6ee4d8cc99aca
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-02-26 08:18:51 -0600

    Create bobThere.js

---
+++ String-2/bobThere.js
@@ -0,0 +1,12 @@
+/*String-2 -- bobThere
+Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+Examples
+
+bobThere('abcbob') → true
+bobThere('b9b') → true
+bobThere('bac') → false*/
+
+function bobThere(str){
+  return /b[\w]b/.test(str);
+}
