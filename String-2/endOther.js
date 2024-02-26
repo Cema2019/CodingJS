9bd94ab5bd0b72ebf6172e265452ffd63c067472
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-02-26 05:55:40 -0600

    Create endOther.js

---
+++ String-2/endOther.js
@@ -0,0 +1,12 @@
+/*String-2 -- endOther
+Given two strings, return true if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string.
+
+Examples
+
+endOther('Hiabc', 'abc') → true
+endOther('AbC', 'HiaBc') → true
+endOther('abc', 'abXabc') → true*/
+
+function endOther(a, b){
+    return a.toLowerCase().endsWith(b.toLowerCase()) || b.toLowerCase().endsWith(a.toLowerCase());
+}
