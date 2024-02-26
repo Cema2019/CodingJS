213ba24ad63004acd68aa3f2ddc63c53044e4bfc
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-02-25 20:08:31 -0600

    Create frontAgain.js

---
+++ String-1/frontAgain.js
@@ -0,0 +1,12 @@
+/*String-1 -- frontAgain
+Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited".
+
+Examples
+
+frontAgain('edited') → true
+frontAgain('edit') → false
+frontAgain('ed') → true*/
+
+function frontAgain(str){
+  return str.length >= 2 ? str.slice(0,2) === str.slice(-2) : false;
+}
