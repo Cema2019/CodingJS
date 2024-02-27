fee9f9db08fd00771b70c5af052e998954c9c1a7
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-02-26 19:50:17 -0600

    Create doubleX.js

---
+++ Warmup-1/doubleX.js
@@ -0,0 +1,12 @@
+/*Warmup-2 -- doubleX
+Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+Examples
+
+doubleX('axxbb') → true
+doubleX('axaxax') → false
+doubleX('xxxxx') → true*/
+
+function doubleX(str){
+  return str[str.indexOf('x') + 1] === 'x'
+}
